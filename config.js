f881fa9d0e636b22eadd0adfc83eed7011df40b6
Данило <danikrut79@gmail.com>
2025-12-31 20:38:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 window.GH_CONFIG = {
-token: '{{GH_PAT}}',
+token: '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',
 owner: 'Danik25326',
 repo: 'pocket_trading_bot',
 workflowId: 'signals.yml'
