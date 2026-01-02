91009c600062d2a0d848b6885cd8878feeb5eb20
Данило <danikrut79@gmail.com>
2026-01-02 17:11:42 +0200

    Update GitHub token in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 window.GH_CONFIG = {
-token: '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',
+token: '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',
 owner: 'Danik25326',
 repo: 'pocket_trading_bot',
 workflowId: 'signals.yml'
