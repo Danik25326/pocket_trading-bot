d7b81dcf39f167f01e9e2cbfe95c701b84e7ecbd
Данило <danikrut79@gmail.com>
2026-01-05 01:01:12 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js - automatically generated
-window.GH_CONFIG = {
-    token: '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',
-    owner: 'Danik25326',
-    repo: 'pocket_trading_bot',
-    workflowId: 'signals.yml'
-};
