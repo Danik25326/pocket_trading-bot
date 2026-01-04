db0fe228d30e18661ab00a0939d14411f12104c1
Данило <danikrut79@gmail.com>
2026-01-04 09:56:47 +0200

    Update GitHub config with new parameters

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 window.GH_CONFIG = {
-token: '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',
-owner: 'Danik25326',
-repo: 'pocket_trading_bot',
-workflowId: 'signals.yml'
+    owner: 'Danik25326',
+    repo: 'pocket_trading_bot',
+    branch: 'main',
+    baseUrl: 'https://danik25326.github.io/pocket_trading_bot'
 };
