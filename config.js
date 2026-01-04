3e581cbe9bef9351d52be0cdb23d9783dbc65eb7
Данило <danikrut79@gmail.com>
2026-01-04 14:16:12 +0200

    Fix formatting in config.js

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ window.GH_CONFIG = {
     owner: 'Danik25326',
     repo: 'pocket_trading_bot',
     branch: 'main',
-    baseUrl: 'https://danik25326.github.io/pocket_trading_bot',
-    token: '{{GH_PAT}}'
+    baseUrl: 'https://danik25326.github.io/pocket_trading_bot'
 };
