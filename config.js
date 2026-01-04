43ac3aa5e608998959a4b2546f045d40484078d1
Данило <danikrut79@gmail.com>
2026-01-04 14:36:23 +0200

    Add GitHub token to GH_CONFIG
    Added GitHub token to configuration.

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ window.GH_CONFIG = {
     owner: 'Danik25326',
     repo: 'pocket_trading_bot',
     branch: 'main',
-    baseUrl: 'https://danik25326.github.io/pocket_trading_bot'
+    baseUrl: 'https://danik25326.github.io/pocket_trading_bot',
+    token: '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' // ← Додайте свій GitHub Token тут!
 };
