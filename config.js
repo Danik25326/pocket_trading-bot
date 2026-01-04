86ea7a3b785945b50b14295d328ee8b7f5445b61
Данило <danikrut79@gmail.com>
2026-01-04 13:21:17 +0200

    Use placeholder for GitHub token in config.js
    Replaced hardcoded GitHub token with placeholder

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.GH_CONFIG = {
     repo: 'pocket_trading_bot',
     branch: 'main',
     baseUrl: 'https://danik25326.github.io/pocket_trading_bot',
-    token: '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'
+    token: '{{GH_PAT}}'
 };
